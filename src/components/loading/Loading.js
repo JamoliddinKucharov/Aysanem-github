f530771b942ed0f0f4b4b7b12f0d94a7fe985c6a
Jamoliddin <jamoliddindev@gmail.com>
2025-03-26 21:14:41 +0500

    loading updated

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -2,9 +2,7 @@ import React from 'react'
 import "./loading.css"
 function Loading() {
     return (
-        <div class="loadingio-spinner-spinner-2by998twmg8"><div class="ldio-yzaezf3dcmj">
-            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-        </div></div>
+        <img src="/assets/images/instalady.png" alt="" className="loadingIcon" />
     )
 }
 
