29fb751de21013986c56013951c2b308f42394ca
Jamoliddin <jamoliddindev@gmail.com>
2025-04-02 23:45:21 +0500

    feat: video

--- src/components/videoBlock/videoBlock.js
+++ src/components/videoBlock/videoBlock.js
@@ -4,22 +4,42 @@ import YouTube from "react-youtube";
 
 const VideoBlock = React.memo(() => {
   const [videoStarted, setVideoStarted] = useState(false);
+  const [videoStartedButton, setVideoStartedButton] = useState(false);
+  const [player, setPlayer] = useState(null);
 
   const opts = {
     height: "100%",
     width: "100%",
     playerVars: {
       autoplay: 1,
-      controls: 0, // Foydalanuvchi boshqaruv elementlarini ko‘rmasligi uchun
-      disablekb: 1, // Klaviatura orqali boshqarish bloklanadi
+      controls: 0,
+      disablekb: 1,
       modestbranding: 1,
       rel: 0,
-      fs: 0, // Fullscreen tugmachasini o‘chiradi
-      iv_load_policy: 3, // Reklama bannerlarini olib tashlashga harakat qiladi
-      preventInteraction: 1, // **📌 Interaktiv harakatlarni bloklaydi (iOS-da)**
-      gesture: "none", // **📌 Mobil qurilmalarda ikki marta bosish orqali oldinga o'tishni bloklaydi**
+      fs: 0,
+      iv_load_policy: 3,
+      preventInteraction: 1,
+      gesture: "none",
     },
   };
+
+  const onReady = (event) => {
+    setPlayer(event.target);
+  };
+
+  const handleOverlayClick = () => {
+    if (player) {
+      const state = player.getPlayerState();
+      if (state === 1) {
+        player.pauseVideo();
+        setVideoStartedButton(true)
+      } else {
+        player.playVideo();
+        setVideoStartedButton(false)
+      }
+    }
+  };
+
 
   return (
     <div className={styles.videoBlock}>
@@ -47,8 +67,28 @@ const VideoBlock = React.memo(() => {
               </div>
             </div>
           ) : (
-            <YouTube videoId="9QIUDujg6Ow" opts={opts} style={{ height: "100%" }} />
+            <div className={styles.videoWrapper}>
+              <YouTube videoId="9QIUDujg6Ow" opts={opts} style={{ height: "100%" }} onReady={onReady} />
+              <div className={styles.overlay} onClick={handleOverlayClick}></div>
+            </div>
           )}
+          {videoStartedButton &&
+            <div className={styles.play} >
+              <svg
+                width="50"
+                height="57"
+                viewBox="0 0 50 57"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M49.8791 28.5166L25.3999 42.6461L0.930664 56.7757V28.5166V0.267578L25.3999 14.397L49.8791 28.5166Z"
+                  fill="white"
+                />
+              </svg>
+            </div>}
         </div>
         <p>
           5 daqiqalik rolikni tomosha qilib, barcha savollaringizga javob oling.
