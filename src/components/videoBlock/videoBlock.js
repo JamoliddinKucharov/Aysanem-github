21fe287e2f12f79ea94a0a33ae6465134ad85a60
Jamoliddin <jamoliddindev@gmail.com>
2025-04-02 19:03:07 +0500

    feat: updated video

--- src/components/videoBlock/videoBlock.js
+++ src/components/videoBlock/videoBlock.js
@@ -3,93 +3,28 @@ import styles from "./videoBlock.module.css";
 import YouTube from "react-youtube";
 
 const VideoBlock = React.memo(() => {
-  const [videoEnded, setVideoEnded] = useState(false);
-
-
+  const [videoStarted, setVideoStarted] = useState(false);
 
   const opts = {
-    height: "390",
-    width: "640",
+    height: "100%",
+    width: "100%",
     playerVars: {
       autoplay: 1,
-      controls: 0, // 🎯 Foydalanuvchi videoni oldinga o‘tkaza olmasligi uchun
-      disablekb: 1, // Klaviatura orqali boshqarishni o‘chiradi
+      controls: 0,
+      disablekb: 1,
       modestbranding: 1,
-      rel: 0, // O‘xshash videolar ko‘rinmasligi uchun
+      rel: 0,
     },
   };
 
-  const onEnd = () => {
-    setVideoEnded(true);
-  };
   return (
     <div className={styles.videoBlock}>
       <div className="container">
         <h1 className={styles.title}>Hayotingizni yaxshilash vaqti keldi!</h1>
         <div className={styles.video}  >
-          {/* {isPlaying ? (
-            <iframe
-              className={styles.videoPlayer}
-              width="100%"
-              height="100%"
-              src="https://www.youtube.com/embed/9QIUDujg6Ow?autoplay=1"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          ) : (
-            <div
-              className={`${styles.play} ${isPlaying ? styles.hidden : ""}`}
-              onClick={(e) => {
-                e.stopPropagation();
-                handlePlayPause();
-              }}
-            >
-              {isPlaying ? (
-                <svg
-                  width="50"
-                  height="50"
-                  viewBox="0 0 50 50"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect x="12" y="10" width="8" height="40" fill="white" />
-                  <rect x="30" y="10" width="8" height="40" fill="white" />
-                </svg>
-              ) : (
-                <svg
-                  width="50"
-                  height="57"
-                  viewBox="0 0 50 57"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M49.8791 28.5166L25.3999 42.6461L0.930664 56.7757V28.5166V0.267578L25.3999 14.397L49.8791 28.5166Z"
-                    fill="white"
-                  />
-                </svg>
-              )}
-            </div> 
-          )} */}
-
-          {/* {isPlaying ? (
-            <iframe
-              className={styles.videoPlayer}
-              width="100%"
-              height="100%"
-              src="https://www.youtube.com/embed/9QIUDujg6Ow?autoplay=1"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          ) : (
-            <>
-              <img src="/assets/images/poster.png" className={styles.poster} alt="Video poster" />
+          {!videoStarted ? (
+            <div className={styles.poster} onClick={() => setVideoStarted(true)}>
+              <img src="/assets/images/poster.png" alt="Poster" className={styles.posterImage} />
               <div className={styles.play} >
                 <svg
                   width="50"
@@ -106,13 +41,9 @@ const VideoBlock = React.memo(() => {
                   />
                 </svg>
               </div>
-            </>
-          )} */}
-
-          {videoEnded ? (
-            <p>Video tugadi!</p> // 🎯 Video tugaganda boshqa tarkib ko‘rsatish mumkin
+            </div>
           ) : (
-            <YouTube videoId="9QIUDujg6Ow" opts={opts} onEnd={onEnd} />
+            <YouTube videoId="9QIUDujg6Ow" opts={opts} style={{ height: "100%" }} />
           )}
         </div>
         <p>
